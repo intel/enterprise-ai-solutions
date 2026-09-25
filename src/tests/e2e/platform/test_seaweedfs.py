732f8615fc59a8570e1125b063b114df186b0295
Jakub Piasecki <jakub.piasecki@intel.com>
2026-09-25 05:33:06 -0700

    test(e2e): report SeaweedFS readiness in Allure as IEASG-T682
    Signed-off-by: Jakub Piasecki <jakub.piasecki@intel.com>

--- src/tests/e2e/platform/test_seaweedfs.py
+++ src/tests/e2e/platform/test_seaweedfs.py
@@ -3,9 +3,11 @@
 # Copyright (C) 2024-2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+import allure
 import kr8s
 
 
+@allure.testcase("IEASG-T682")
 def test_seaweedfs_s3_ready():
     deploys = list(kr8s.get("deployments", namespace="monitoring",
                             field_selector={"metadata.name": "seaweedfs-telemetry-s3"}))
